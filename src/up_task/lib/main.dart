efe6b350466a7ced8560740964151e2decd19189
Priyanshu <ps3556911@gmail.com>
2023-09-23 12:40:21 +0530

    Changed Title of Home Page

--- src/up_task/lib/main.dart
+++ src/up_task/lib/main.dart
@@ -15,7 +15,9 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(
+        title: 'UpTask',
+      ),
     );
   }
 }
@@ -36,6 +38,7 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
+        // centerTitle: true,
       ),
       body: Center(
         child: Column(
