986148d46f526d26bdfeec42c705335b7868ad2a
sauravdani <sauravdani7@gmail.com>
2023-09-26 21:38:30 +0530

    recurrence tile

--- src/up_task/lib/customwidgets/Recurrence/recurrence_tile.dart
+++ src/up_task/lib/customwidgets/Recurrence/recurrence_tile.dart
@@ -5,8 +5,23 @@ class RecurrenceTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      
-    );
+    return Padding(         //Adding padding around list of options
+      padding: const EdgeInsets.all(20),
+      child: Container(
+        padding: EdgeInsets.all(20),
+        child: Row(
+          children: [
+            //check box
+            Checkbox(value: value, onChanged: onChanged)
+
+            //task name
+            Text("Do not Repeat"),
+          ],
+        ),
+        decoration: BoxDecoration(borderRadius: BorderRadius.circular(12),
+        color: const Color.fromARGB(255, 158, 158, 158)),
+
+      );,
+    )
   }
 }
