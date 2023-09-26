440c838bca70242145c1ca827a23cc3086fab04d
sauravdani <sauravdani7@gmail.com>
2023-09-26 23:01:20 +0530

    added checkbox arguments in recurrence tile

--- src/up_task/lib/customwidgets/Recurrence/recurrence_tile.dart
+++ src/up_task/lib/customwidgets/Recurrence/recurrence_tile.dart
@@ -1,27 +1,35 @@
 import 'package:flutter/material.dart';
 
 class RecurrenceTile extends StatelessWidget {
-  const RecurrenceTile({super.key});
+  final bool ticked;
+  Function(bool?)? onChanged;
+
+  RecurrenceTile({
+    super.key,
+    required this.ticked,
+    required this.onChanged,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Padding(         //Adding padding around list of options
+    return Padding(
+      //Adding padding around list of options
       padding: const EdgeInsets.all(20),
       child: Container(
         padding: EdgeInsets.all(20),
         child: Row(
           children: [
             //check box
-            Checkbox(value: value, onChanged: onChanged),
+            Checkbox(value: ticked, onChanged: onChanged),
 
             //task name
             Text("Do not Repeat"),
           ],
         ),
-        decoration: BoxDecoration(borderRadius: BorderRadius.circular(12),
-        color: const Color.fromARGB(255, 158, 158, 158)),
-
-      );,
-    )
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(12),
+            color: const Color.fromARGB(255, 158, 158, 158)),
+      ),
+    );
   }
 }
