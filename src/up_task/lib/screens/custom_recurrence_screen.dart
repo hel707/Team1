4614b906e7c1504ef40279ea29bbc9c14d987a8e
sauravdani <sauravdani7@gmail.com>
2023-09-28 12:49:48 +0530

    Update custom_recurrence_screen.dart

--- src/up_task/lib/screens/custom_recurrence_screen.dart
+++ src/up_task/lib/screens/custom_recurrence_screen.dart
@@ -8,35 +8,43 @@ class CustomTask extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: Text("Custom Recurrence")),
+//main column
         body: Column(
           children: [
+//Repeat every
             Row(
               children: [
                 Padding(
-                  padding: const EdgeInsets.only(left: 15, right: 30, top: 15, bottom: 10),
-                  child: Text('Repeats Every', style: TextStyle(fontSize: 15),),
+                  padding: const EdgeInsets.only(
+                      left: 15, right: 30, top: 15, bottom: 10),
+                  child: Text(
+                    'Repeats Every',
+                    style: TextStyle(fontSize: 15),
+                  ),
                 ),
               ],
             ),
+
+//date selection text field and drop down
             Padding(
-              padding: const EdgeInsets.only(left: 5, top: 5,bottom: 20),
+              padding: const EdgeInsets.only(left: 5, top: 5, bottom: 20),
               child: Row(
                 children: [
                   Container(
                     width: 50, // Set your desired width here
-                    child: TextField(
-                      decoration: InputDecoration(
-                        hintText: '1',
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 10, right: 10),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          hintText: '1',
+                        ),
                       ),
                     ),
                   ),
-            
-                  SizedBox(
-                      width: 16),
+                  SizedBox(width: 16),
                   DropdownButton<String>(
-                    value: 'Day', 
-                    onChanged: (newValue) {
-                    },
+                    value: 'Day',
+                    onChanged: (newValue) {},
                     items: <String>['Day', 'Week', 'Month', 'Year']
                         .map<DropdownMenuItem<String>>((String value) {
                       return DropdownMenuItem<String>(
@@ -45,11 +53,32 @@ class CustomTask extends StatelessWidget {
                       );
                     }).toList(),
                   ),
-            
-            //week selection
-                  
                 ],
               ),
+            ),
+//week selection
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(
+                      left: 15, right: 30, top: 15, bottom: 10),
+                  child: Text(
+                    "Repeats on",
+                    style: TextStyle(fontSize: 15),
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                FloatingActionButton(heroTag: "M", onPressed: null,child: Text("M")),
+                FloatingActionButton(heroTag: "T", onPressed: null,child: Text("T")),
+                FloatingActionButton(heroTag: "W", onPressed: null,child: Text("W")),
+                FloatingActionButton(heroTag: "T", onPressed: null,child: Text("T")),
+                FloatingActionButton(heroTag: "F", onPressed: null,child: Text("F")),
+                FloatingActionButton(heroTag: "S", onPressed: null,child: Text("S")),
+                FloatingActionButton(heroTag: "Su", onPressed: null,child: Text("S")),
+              ],
             )
           ],
         ),
