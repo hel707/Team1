fd5f42e8cb27a164a1b613d3154f98b73b4a9743
Priyanshu Suryawanshi <ps3556911@gmail.com>
2023-09-29 00:58:53 +0530

    Added notification page

---
+++ src/up_task/lib/data/notification_servies.dart
@@ -0,0 +1,59 @@
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+class NotificationServices {
+  final FlutterLocalNotificationsPlugin _flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+
+  final AndroidInitializationSettings _androidInitializationSettings =
+      AndroidInitializationSettings('@mipmap/ic_launcher');
+
+  void initializeNotificaions() async {
+    InitializationSettings initializationSettings =
+        InitializationSettings(android: _androidInitializationSettings);
+    await _flutterLocalNotificationsPlugin.initialize(initializationSettings);
+  }
+
+  void sendNotifications(String title, String body) async {
+    AndroidNotificationDetails androidNotificationDetails =
+        AndroidNotificationDetails('channelId', 'channelName',
+            priority: Priority.high, importance: Importance.high);
+
+    NotificationDetails notificationDetails = NotificationDetails(
+      android: androidNotificationDetails,
+    );
+
+    await _flutterLocalNotificationsPlugin.show(
+        0, title, body, notificationDetails);
+  }
+
+  void scheduleNotifications(String title, String body) async {
+    AndroidNotificationDetails androidNotificationDetails =
+        AndroidNotificationDetails('channelId', 'channelName',
+            priority: Priority.high, importance: Importance.high);
+
+    NotificationDetails notificationDetails = NotificationDetails(
+      android: androidNotificationDetails,
+    );
+
+    await _flutterLocalNotificationsPlugin.periodicallyShow(0, title, body,
+        RepeatInterval.values as RepeatInterval, notificationDetails);
+  }
+
+  void stopNotifications() async {
+    _flutterLocalNotificationsPlugin.cancel(0);
+  }
+}
+
+
+  // NotificationServices notificationServices = NotificationServices();
+
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   notificationServices.initializeNotificaions();
+  // }
+
+
+// onPressed: () {
+//           notificationServices.sendNotifications('title', 'body');
+//         },
