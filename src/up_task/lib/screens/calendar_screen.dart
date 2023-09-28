3afbda2d7da024f8adcfe82e67a76664438cdcb6
Shivendu <Shivendukumarsuman@gmail.com>
2023-09-28 13:42:06 +0530

    Added Calendar Screen

--- src/up_task/lib/screens/calendar_screen.dart
+++ src/up_task/lib/screens/calendar_screen.dart
@@ -1,19 +1,90 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class CalendarScreen extends StatefulWidget {
-  const CalendarScreen({super.key});
+  const CalendarScreen({Key? key}) : super(key: key);
 
   @override
   State<CalendarScreen> createState() => _CalendarScreenState();
 }
 
 class _CalendarScreenState extends State<CalendarScreen> {
+  CalendarFormat _calendarFormat = CalendarFormat.month;
+  DateTime _focusedDay = DateTime.now();
+  DateTime? _selectedDay;
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[Text("Calendar Screen")],
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text("Calendar "),
+      ),
+      body: TableCalendar(
+        firstDay: DateTime.utc(2000, 01, 01),
+        lastDay: DateTime(2030, 01, 01),
+        focusedDay: _focusedDay,
+        calendarFormat: _calendarFormat,
+        startingDayOfWeek: StartingDayOfWeek.monday,
+        rowHeight: 60,
+        daysOfWeekHeight: 60,
+        headerStyle: HeaderStyle(
+          titleTextStyle:
+              const TextStyle(color: Colors.teal, fontWeight: FontWeight.bold),
+          formatButtonTextStyle: const TextStyle(color: Colors.green),
+          formatButtonDecoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            border: Border.all(color: Colors.teal, width: 2),
+          ),
+          leftChevronIcon: const Icon(
+            Icons.arrow_back,
+            color: Colors.teal,
+            size: 28,
+          ),
+          rightChevronIcon: const Icon(
+            Icons.arrow_forward,
+            color: Colors.teal,
+            size: 28,
+          ),
+        ),
+        daysOfWeekStyle: const DaysOfWeekStyle(
+          weekendStyle: TextStyle(color: Colors.red),
+        ),
+        calendarStyle: const CalendarStyle(
+          weekendTextStyle: TextStyle(color: Colors.red),
+          todayDecoration: BoxDecoration(
+            color: Colors.amber,
+            shape: BoxShape.circle,
+          ),
+          selectedDecoration: BoxDecoration(
+            color: Colors.teal,
+            shape: BoxShape.circle,
+          ),
+        ),
+        onDaySelected: (selectedDay, focusedDay) {
+          if (!isSameDay(_selectedDay, selectedDay)) {
+            // Call `setState()` when updating the selected day
+            setState(() {
+              _selectedDay = selectedDay;
+              _focusedDay = focusedDay;
+            });
+          }
+        },
+        selectedDayPredicate: (day) {
+          return isSameDay(_selectedDay, day);
+        },
+        onFormatChanged: (format) {
+          if (_calendarFormat != format) {
+            // Call `setState()` when updating calendar format
+            setState(() {
+              _calendarFormat = format;
+            });
+          }
+        },
+        onPageChanged: (focusedDay) {
+          // No need to call `setState()` here
+          _focusedDay = focusedDay;
+        },
       ),
     );
   }
